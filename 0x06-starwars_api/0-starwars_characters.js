4d07e32f3bd60f4496b59596030858410dc2b0d9
OUALIID <walidelhadim8@gmail.com>
2024-02-16 02:47:59 +0100

    script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv.slice(2);
-const url = `https://swapi-api.alx-tools.com/api/films/${id[0]}/`;
+const id = process.argv[2]; // Changed to directly access the first argument
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 request(url, (err, response, body) => {
   if (err) {
+    console.error(err);
     return;
   }
-  const dates = JSON.parse(body).characters;
-  dates.forEach(date => {
-    request(date, (dateErr, dateResponse, dateBody) => {
-      if (date) {
-        console.log(JSON.parse(dateBody).name);
+  const characters = JSON.parse(body).characters;
+  characters.forEach(characterUrl => {
+    request(characterUrl, (characterErr, characterResponse, characterBody) => {
+      if (characterErr) {
+        console.error(characterErr);
+        return;
       }
+      console.log(JSON.parse(characterBody).name);
     });
   });
 });
