7a21a882448b87944f7307208b19b38e847b66d8
OUALIID <walidelhadim8@gmail.com>
2024-02-16 02:56:44 +0100

    script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,32 @@
 #!/usr/bin/node
+
 const request = require('request');
-const id = process.argv[2]; // Changed to directly access the first argument
-const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
-request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Please provide a movie ID');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.error('Failed to fetch movie information:', error || response.statusCode);
     return;
   }
+
   const characters = JSON.parse(body).characters;
+
   characters.forEach(characterUrl => {
-    request(characterUrl, (characterErr, characterResponse, characterBody) => {
-      if (characterErr) {
-        console.error(characterErr);
+    request.get(characterUrl, (charError, charResponse, charBody) => {
+      if (charError || charResponse.statusCode !== 200) {
+        console.error('Failed to fetch character information:', charError || charResponse.statusCode);
         return;
       }
-      console.log(JSON.parse(characterBody).name);
+
+      console.log(JSON.parse(charBody).name);
     });
   });
 });
