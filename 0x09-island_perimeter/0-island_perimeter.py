74ba4f277d1d3cfba7be2d655561139725e82c54
OUALIID <walidelhadim8@gmail.com>
2024-03-05 06:49:33 +0100

    A function that returns the circumference of the island described in the grid.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,8 +13,10 @@ def island_perimeter(grid):
         for column in range(len(grid[0])):
             if grid[row][column] == 1:
                 perimeter += 4
-                if row and column > 0 and \
-                    grid[row - 1][column] == 1 \
-                        or grid[row][column - 1] == 1:
+                if row > 0 and grid[row - 1][column] == 1:
+                    perimeter -= 2
+                if column > 0 and grid[row][column - 1] == 1:
                     perimeter -= 2
     return perimeter
+            
+
