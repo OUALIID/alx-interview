62679f1e87181deb7add50f700ea4b0b79434643
OUALIID <walidelhadim8@gmail.com>
2024-03-13 14:54:07 +0000

    try to fix my code 1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,29 +9,24 @@ def isWinner(x, nums):
     if x < 1 or not nums:
         return None
 
-    maria = 0
-    ben = 0
+    m_wins = 0
+    b_wins = 0
+
+    n = max(nums)
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+
+    for x in range(2, int(n**0.5) + 1):
+        if primes[x]:
+            for y in range(x**2, n + 1, x):
+                primes[y] = False
 
     for n in nums:
-        # Generate a list of prime numbers
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
+        count = sum(primes[2:n+1])
+        b_wins += count % 2 == 0
+        m_wins += count % 2 == 1
 
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-
-        # Check if there's at least one prime number left
-        if any(primes[i] for i in range(2, n + 1)):
-            if n % 2 == 0:  # Even round number, Ben's turn
-                ben += 1
-            else:  # Odd round number, Maria's turn
-                maria += 1
-
-    if maria > ben:
-        return "Maria"
-    elif ben > maria:
-        return "Ben"
-    else:
+    if m_wins == b_wins:
         return None
+
+    return 'Maria' if m_wins > b_wins else 'Ben'
