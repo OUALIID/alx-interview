b4b3327b1276c0ec5c22aa075a60633ea8d691ad
OUALIID <walidelhadim8@gmail.com>
2024-03-13 14:17:25 +0000

    A function to determine the winner in a prime numbers game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    primes_count = 0
+    maria = 0 
+
+    for n in nums:
+        for i in range(2, n + 1):
+            if all(i % j != 0 for j in range(2, int(i ** 0.5) + 1)):
+                primes_count += 1
+        if primes_count % 2 != 0:
+            maria += 1
+
+    ben = len(nums) - maria
+
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    else:
+        return None
